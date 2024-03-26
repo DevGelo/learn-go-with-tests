d40b3a2b0b87115be47dad9425767912afa2646e
DevGelo <DevGelo@users.noreply.github.com>
2024-03-26 16:42:25 +0300

    Create hello_test.go

---
+++ 01 - hello-world/v04/hello_test.go
@@ -0,0 +1,22 @@
+// https://go.dev/play/
+
+package main
+
+import (
+	"testing"
+)
+
+const englishHelloPrefix = "Hello, "
+
+func Hello(name string) string {
+	return englishHelloPrefix + name
+}
+
+func TestLastIndex(t *testing.T) {
+	got := Hello("Mike")
+	want := "Hello, Mike"
+
+	if got != want {
+		t.Errorf("want %q got %q", want, got)
+	}
+}
