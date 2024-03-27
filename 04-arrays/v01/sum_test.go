de7d77c1e7646d1697cdf94b82dd9f0a880892e6
DevGelo <DevGelo@users.noreply.github.com>
2024-03-27 12:04:15 +0300

    Create sum_test.go

---
+++ 04-arrays/v01/sum_test.go
@@ -0,0 +1,28 @@
+// https://go.dev/play/
+
+package main
+
+import (
+	"testing"
+)
+
+// Sum calculates the total from an array of numbers.
+func Sum(numbers [5]int) int {
+	sum := 0
+	for i := 0; i < 5; i++ {
+		sum += numbers[i]
+	}
+
+	return sum
+}
+
+func TestSum(t *testing.T) {
+	numbers := [...]int{1, 2, 3, 5, 7}
+
+	got := Sum(numbers)
+	want := 18
+
+	if got != want {
+		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	}
+}
