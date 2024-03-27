bb1e2d6a55b439f058eb13a243c51c20f178390e
DevGelo <DevGelo@users.noreply.github.com>
2024-03-27 12:15:49 +0300

    Update sum_test.go

--- 04-arrays/v03/sum_test.go
+++ 04-arrays/v03/sum_test.go
@@ -17,16 +17,6 @@ func Sum(numbers []int) int {
 }
 
 func TestSum(t *testing.T) {
-	t.Run("Sum collection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 5, 7}
-
-		got := Sum(numbers)
-		want := 18
-
-		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
-		}
-	})
 	t.Run("Sum collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 
